68555618a478187acd520d92662f4f7c9ae2e8c8
yyysolhhh <solbaram37@gmail.com>
2024-06-03 23:27:35 +0900

    명령어 입력 처리

--- js/commands.js
+++ js/commands.js
@@ -1,7 +1,17 @@
-help = [
-  "whois      Who is yyysolhhh?",
-  "github     Display github link",
-  "email      Display email address",
-  "projects   View projects",
-  "secret     Find the password",
+const help = [
+  "whois      소개",
+  "github     깃허브 주소",
+  "email      이메일 주소",
+  "blog       기술블로그 주소",
+  "projects   프로젝트 리스트",
+  "study      학습",
+  "algo       알고리즘",
+  "clear      화면 초기화",
+  // "secret     Find the password",
 ];
+
+const whois = ["안녕하세요! 백엔드 개발자 이예솔하입니다."];
+
+const github = ["https://github.com/yyysolhhh"];
+
+const algo = ["https://github.com/yyysolhhh/PS_study"];
--- js/main.js
+++ js/main.js
@@ -1,57 +1,181 @@
-const og = document.getElementById("og");
-const lines = document.getElementById("lines");
-const command = document.getElementById("commander");
-const textarea = document.getElementById("texter");
+const container = document.getElementById("container");
+const currentDirectory = document.getElementById("dir");
 const terminal = document.getElementById("terminal");
-let commands = [];
-let git = 0;
+const input = document.getElementById("input");
+let inputCounter = 0;
 
-function command(cmd) {
-  switch (cmd.toLowerCase()) {
+container.addEventListener("click", () => {
+  input.focus();
+});
+
+function terminalInput(event) {
+  if (event.key === "Tab") {
+    event.preventDefault();
+    input.focus();
+  }
+
+  const focusElement = document.activeElement.className;
+  const isTyping = focusElement == "console-input";
+  let isPrintableKey = event.key.length === 1;
+
+  if (isTyping) {
+    if (event.code == "Backspace") {
+      if (inputCounter > 0) {
+        inputCounter = inputCounter - 1;
+        document.querySelector(".blink").style.transform = "translateX(" + (inputCounter * 10).toString() + "px)";
+      }
+    } else if (isPrintableKey) {
+      inputCounter = inputCounter + 1;
+      document.querySelector(".blink").style.transform = "translateX(" + (inputCounter * 10).toString() + "px)";
+    }
+  }
+}
+
+const addLine = (text, className) => {
+  let t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+
+  let tag = document.createElement("p");
+  tag.className = className || "new-line";
+  tag.innerHTML = t;
+  terminal.append(tag);
+  tag.scrollIntoView();
+};
+
+const addMultiLine = (command, className) => {
+  command.map((line) => addLine(line, className));
+};
+
+const addOldLine = () => {
+  let tag = document.createElement("div");
+  tag.className = "prompt";
+
+  let guestDiv = document.createElement("div");
+  let guestText = document.createTextNode("guest@yyysolhhh\u00A0");
+  guestDiv.appendChild(guestText);
+  guestDiv.className = "green";
+
+  let inDiv = document.createElement("div");
+  let inText = document.createTextNode(``);
+  inDiv.appendChild(inText);
+
+  let folderDiv = document.createElement("div");
+  let folderText = document.createTextNode(currentDirectory.innerText);
+  folderDiv.appendChild(folderText);
+  folderDiv.className = "purple";
+
+  tag.appendChild(guestDiv);
+  tag.appendChild(inDiv);
+  tag.appendChild(folderDiv);
+
+  let tag2 = document.createElement("div");
+  tag2.className = "prompt";
+
+  let arrowDiv = document.createElement("div");
+  let arrowText = document.createTextNode("$\u00A0");
+  arrowDiv.appendChild(arrowText);
+  arrowDiv.className = "start-point";
+
+  let commandDiv = document.createElement("div");
+  let commandText = document.createTextNode(`${input.value}`);
+  commandDiv.appendChild(commandText);
+
+  tag2.appendChild(arrowDiv);
+  tag2.appendChild(commandDiv);
+
+  terminal.append(tag);
+  terminal.append(tag2);
+};
+
+const resetCommand = () => {
+  input.value = "";
+  document.querySelector(".blink").style.transform = "translateX(" + (1).toString() + "px)";
+  inputCounter = 0;
+};
+
+const clear = () => {
+  terminal.innerHTML = ``;
+  document.querySelector(".presentation").innerHTML = ``;
+  document.querySelector(".presentation-text").innerHTML = ``;
+};
+
+const checkForCommand = () => {
+  switch (input.value) {
     case "help":
-      help;
+      addMultiLine(help, "new-line");
       break;
     case "whois":
-      whois;
+      addMultiLine(whois, "new-line");
       break;
-    case "github":
-      github;
-      break;
-    case "email":
-      email;
-      break;
-    case "projects":
-      projects;
-      break;
-    case "secret":
-      secret;
+    case "clear":
+      clear();
       break;
   }
-}
+};
 
-function enterKey(e) {
-  if (e.keyCode == 181) {
-    document.location.reload(true);
+window.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    addOldLine();
+    checkForCommand();
+    resetCommand();
   }
-  if (e.keyCode == 13) {
-    commands.push(command.innerHTML);
-    git = commands.length;
-    liner(command.innerHTML, "no-animation"), commander(command.innerHTML.toLowerCase());
-    command.innerHTML = "";
-    textarea.value = "";
-  }
-  if (e.keyCode == 38 && git != 0) {
-    git -= 1;
-    textarea.value = commands[git];
-    command.innerHTML = textarea.value;
-  }
-  if (e.keyCode == 40 && git != commands.length) {
-    git += 1;
-    if (commands[git] === undefined) {
-      textarea.value = "";
-    } else {
-      textarea.value = commands[git];
-    }
-    command.innerHTML = textarea.value;
-  }
-}
+});
+
+window.addEventListener("keydown", terminalInput);
+
+// function command(cmd) {
+//   switch (cmd.toLowerCase()) {
+//     case "help":
+//       help;
+//       break;
+//     case "whois":
+//       whois;
+//       break;
+//     case "github":
+//       github;
+//       break;
+//     case "email":
+//       email;
+//       break;
+//     case "projects":
+//       projects;
+//       break;
+//     case "secret":
+//       secret;
+//       break;
+//   }
+// }
+
+// function enterKey(e) {
+//   if (e.keyCode == 181) {
+//     document.location.reload(true);
+//   }
+//   if (e.keyCode == 13) {
+//     commands.push(command.innerHTML);
+//     git = commands.length;
+//     liner(command.innerHTML, "no-animation"), commander(command.innerHTML.toLowerCase());
+//     command.innerHTML = "";
+//     textarea.value = "";
+//   }
+//   if (e.keyCode == 38 && git != 0) {
+//     git -= 1;
+//     textarea.value = commands[git];
+//     command.innerHTML = textarea.value;
+//   }
+//   if (e.keyCode == 40 && git != commands.length) {
+//     git += 1;
+//     if (commands[git] === undefined) {
+//       textarea.value = "";
+//     } else {
+//       textarea.value = commands[git];
+//     }
+//     command.innerHTML = textarea.value;
+//   }
+// }
